8b4ff212cd069aca5fbd90389bcef00c1255e98a
Darshit Kakadiya <darshit.kakadiya@shunyavkash.com>
2026-01-09 12:16:32 +0530

    test cases

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,9 @@
 import { timeAgo } from "../src/index.js";
 
+// Test cases
+console.log(timeAgo(new Date(Date.now() - 31536000000))); // 1 year ago
+console.log(timeAgo(new Date(Date.now() - 2592000000))); // 1 month ago
+console.log(timeAgo(new Date(Date.now() - 86400000))); // 1 day ago
 console.log(timeAgo(new Date(Date.now() - 60000))); // 1 minute ago
 console.log(timeAgo(new Date(Date.now() - 3600000))); // 1 hour ago
 console.log(timeAgo(new Date(Date.now() - 1000))); // just now
