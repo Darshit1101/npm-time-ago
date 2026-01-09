854aa5e41d49b0fa92cfd1dd9a9589bb57d6bcd2
Darshit Kakadiya <darshit.kakadiya@shunyavkash.com>
2026-01-09 14:44:58 +0530

    Your descriptive commit message

--- test/index.cjs.test.js
+++
@@ -1,9 +0,0 @@
-const { timeAgo } = require("../src/index.cjs");
-
-// Test cases
-console.log(timeAgo(new Date(Date.now() - 31536000000))); // 1 year ago
-console.log(timeAgo(new Date(Date.now() - 2592000000))); // 1 month ago
-console.log(timeAgo(new Date(Date.now() - 86400000))); // 1 day ago
-console.log(timeAgo(new Date(Date.now() - 60000))); // 1 minute ago
-console.log(timeAgo(new Date(Date.now() - 3600000))); // 1 hour ago
-console.log(timeAgo(new Date(Date.now() - 1000))); // just now
